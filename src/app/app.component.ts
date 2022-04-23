2df51ff7b7482119d8902769d6920db0ca917752
AfamsVal <progfams@gmail.com>
2022-04-23 05:12:08 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,42 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'team-app';
+  newMember: string = '';
+  members: string[] = [];
+  teamDetails: string[] = [];
+  inputError: string = '';
+  teamDetailError: string = '';
+  teamDetailsInput: string = '';
+
+  addMember = () => {
+    if (this.newMember.trim().length === 0) {
+      this.inputError = 'Name is required!';
+      return;
+    }
+    this.members.push(this.newMember);
+    this.newMember = '';
+  };
+
+  addTeamDetails = () => {
+    if (this.teamDetailsInput.trim().length === 0) {
+      this.teamDetailError = 'Name is required!';
+      return;
+    }
+    this.teamDetails.push(this.teamDetailsInput);
+    this.teamDetailsInput = '';
+  };
+
+  addNameInput(value: string) {
+    this.inputError = '';
+    this.newMember = value;
+  }
+
+  setTeam(value: string) {
+    this.teamDetailError = '';
+    this.teamDetailsInput = value;
+  }
 }
---
+++ src/app/teams/teams.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-teams',
+  templateUrl: './teams.component.html',
+  styleUrls: ['./teams.component.css'],
+})
+export class TeamsComponent implements OnInit {
+  @Input() team: string[] = [];
+  @Input() index: number = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
